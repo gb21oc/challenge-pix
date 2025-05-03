2d271723d24a06a4989fe52ab2b6324fba8657bb
gb21oc <luckygb21@gmail.com>
2025-05-03 13:37:10 -0300

    refactor: alterando o import para index

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
-import { FastifyConstants } from './common/constants/fastify.constants';
 import { FastifyAdapter } from '@nestjs/platform-fastify';
+
+import { FastifyConstants } from './common/constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, new FastifyAdapter());
