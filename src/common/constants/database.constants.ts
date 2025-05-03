3996f18da26153e28db5bfb362713c67d8b3b74c
gb21oc <luckygb21@gmail.com>
2025-05-03 14:16:00 -0300

    refactor: modificando o path das  entidades e migrações para ficar dinamico

--- src/common/constants/database.constants.ts
+++ src/common/constants/database.constants.ts
@@ -1,12 +1,10 @@
-import { TypeOrmModuleOptions } from "@nestjs/typeorm"
-
-export const CONFIG_DATABASE = () => ({
+export const CONFIG_DATABASE = <T>(): T => ({
     type: process.env.DATABASE_TYPE,
     host: process.env.DATABASE_HOST,
     port: process.env.DATABASE_PORT,
     username: process.env.DATABASE_USER,
     password: process.env.DATABASE_PWD,
     database: process.env.DATABASE,
-    entities: [],
-    synchronize: true,
-} as TypeOrmModuleOptions)
+    entities: ['dist/**/*.entity.js'],
+    migrations: ['dist/infra/database/migrations/*.js'],
+} as T)
