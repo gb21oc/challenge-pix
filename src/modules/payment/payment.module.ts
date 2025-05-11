aaab7a396ab6b5f50674adde762e6c48d646f0b7
gb21oc <luckygb21@gmail.com>
2025-05-11 10:59:35 -0300

    feat: criado modulo de pagamento

---
+++ src/modules/payment/payment.module.ts
@@ -0,0 +1,17 @@
+/*
+https://docs.nestjs.com/modules
+*/
+
+import { Module } from '@nestjs/common';
+
+import { PaymentService } from './payment.service';
+import { PaymentController } from './payment.controller';
+
+@Module({
+    imports: [],
+    controllers: [
+        PaymentController,],
+    providers: [
+        PaymentService,],
+})
+export class PaymentModule { }
