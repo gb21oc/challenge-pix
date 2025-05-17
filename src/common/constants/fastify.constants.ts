a8bbe926545cc7e41eab040add2d71027b0451e1
gb21oc <luckygb21@gmail.com>
2025-05-17 16:57:22 -0300

    fix: corrigido texto que não estava sendo carregado

--- src/common/constants/fastify.constants.ts
+++ src/common/constants/fastify.constants.ts
@@ -1,4 +1,4 @@
 export namespace FastifyConstants {
   export const PORT = () => process.env.PORT;
-  export const RUNNING = `Service is running in http://localhost:${PORT()}/`
+  export const RUNNING = () => `Service is running in http://localhost:${PORT()}/`
 }
