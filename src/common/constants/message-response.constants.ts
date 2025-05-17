399463a12cbf2c7574ac2badf7f07b9b113d61f4
gb21oc <luckygb21@gmail.com>
2025-05-17 16:57:35 -0300

    feat: add novas mensagens

--- src/common/constants/message-response.constants.ts
+++ src/common/constants/message-response.constants.ts
@@ -1,3 +1,6 @@
 export const REQUIRED_USER = "Necessário informar o usuário"
 export const REQUIRED_TRADER = "Necessário informar o logista"
 export const DEFAULT_MESSAGE = 'Não foi possível concluir a operação. Verifique os dados.'
+export const AMOUNT_IS_NOT_ZERO = "Valor do PIX não pode ser zero!"
+export const PIX_EXCEEDS_ACOUNT_AMOUNT = "O valor do pix excede o valor da conta."
+export const PIX_IS_NOT_NEGATIVE = "O valor do pix não pode ser negativo"
