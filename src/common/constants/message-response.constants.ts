3538d62fa5e53a2ffb388cd2f2786361cbccbb82
gb21oc <luckygb21@gmail.com>
2025-05-18 16:13:49 -0300

    feat: add novas constants

--- src/common/constants/message-response.constants.ts
+++ src/common/constants/message-response.constants.ts
@@ -4,3 +4,5 @@ export const DEFAULT_MESSAGE = 'Não foi possível concluir a operação. Verifi
 export const AMOUNT_IS_NOT_ZERO = "Valor do PIX não pode ser zero!"
 export const PIX_EXCEEDS_ACOUNT_AMOUNT = "O valor do pix excede o valor da conta."
 export const PIX_IS_NOT_NEGATIVE = "O valor do pix não pode ser negativo"
+export const PIX_NOT_FOUND = "Não foi possível processar o pix informado"
+export const PIX_EXPIRED = "O pix expirou, por gentileza, inicie um novo!"
--- src/common/constants/util.constants.ts
+++ src/common/constants/util.constants.ts
@@ -1,1 +1,6 @@
 export const SALT_ROUNDS = 10
+
+/**
+ * Expira em 3 minutos
+ */
+export const EXPIRE_AT_PIX = () => Date.now() + 3 * 60 * 1000
